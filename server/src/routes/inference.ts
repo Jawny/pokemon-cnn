126850798f49001e2dd7e122dbe0ca9500583f95
Johnny Liao <johnnyliao06@gmail.com>
2022-10-30 15:32:12 -0700

    added weak error checks

--- server/src/routes/inference.ts
+++ server/src/routes/inference.ts
@@ -12,16 +12,20 @@ inferenceRoute.post(
   "/classify-pokemon",
   upload.single("image"),
   async (req: any, res: any) => {
-    console.log(req.file);
-    const { path } = req.file;
+    try {
+      console.log(req.file);
+      const { path } = req.file;
 
-    const model = await tf.loadGraphModel(
-      "file:///home/johnny/code/pokemon-cnn/training/results/tfjs/model.json"
-    );
-
-    const tensorImage = convertImageToTensor(`./${path}`);
-    const tensorPrediction = model.predict(tensorImage);
-    const result = tensorPrediction.argMax(1).arraySync()[0];
-    res.send({ pokemon: POKEMON_LABELS[result] });
+      const model = await tf.loadGraphModel(
+        "file:///home/johnny/code/pokemon-cnn/training/results/tfjs/model.json"
+      );
+      const tensorImage = convertImageToTensor(`./${path}`);
+      const tensorPrediction = model.predict(tensorImage);
+      const result = tensorPrediction.argMax(1).arraySync()[0];
+      res.send({ pokemon: POKEMON_LABELS[result] });
+    } catch (error) {
+      console.error(error);
+      res.send({ error: "null" });
+    }
   }
 );
--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,11 +1,16 @@
+import { Tensor } from "@tensorflow/tfjs-node";
 import * as fs from "fs";
 const tf = require("@tensorflow/tfjs-node");
 
 export const convertImageToTensor = (path: string) => {
-  const imageBuffer = fs.readFileSync(path);
-  const tensor = tf.node.decodeImage(imageBuffer);
-  const resizedTensor = tf.image.resizeBilinear(tensor, [300, 300]);
-  const expandDimsTensor = tf.expandDims(resizedTensor, 0);
+  try {
+    const imageBuffer = fs.readFileSync(path);
+    const tensor = tf.node.decodeImage(imageBuffer);
+    const resizedTensor = tf.image.resizeBilinear(tensor, [300, 300]);
+    const expandDimsTensor = tf.expandDims(resizedTensor, 0);
 
-  return expandDimsTensor;
+    return expandDimsTensor;
+  } catch (error) {
+    return null;
+  }
 };
